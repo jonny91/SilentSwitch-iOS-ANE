218f196b0391c8a4c3d8cc701eedd81d7e44c22e
Jonny.Hong <jonny.hong91@gmail.com>
2015-10-13 11:19:10 +0800

    ANE构建

---
+++ build/make.sh
@@ -0,0 +1,19 @@
+/Applications/Adobe\ Flash\ Builder\ 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt -package -storetype pkcs12 -keystore /Users/Jonny/Documents/ANELibrary/SilentSwitch/build/android.p12 -storepass 12345 -target ane /Users/Jonny/Documents/ANELibrary/SilentSwitch/build/cn.doudou.ane.silentswitch.ane /Users/Jonny/Documents/ANELibrary/SilentSwitch/build/extension.xml -swc /Users/Jonny/Documents/ANELibrary/SilentSwitch/build/SilentSwitchAS.swc -platform iPhone-ARM -C /Users/Jonny/Documents/ANELibrary/SilentSwitch/build/iPhone-ARM .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
